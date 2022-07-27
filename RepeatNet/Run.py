9b7a7c1dd4e50b6fbba7d98075cc4ee575c10334
RyuShige <66407509+RyuShige@users.noreply.github.com>
2022-07-27 13:37:34 +0900

    delete trainer.serialize()

--- RepeatNet/Run.py
+++ RepeatNet/Run.py
@@ -49,7 +49,7 @@ def train(args):
     trainer = CumulativeTrainer(model, None, None, args.local_rank, 4)
     model_optimizer = optim.Adam(model.parameters())
 
-    early_stopping = EarlyStopping(patience=10, verbose=True, path=os.path.join(base_output_path, 'best_model/'))
+    early_stopping = EarlyStopping(patience=10, verbose=True, path=os.path.join(base_output_path, 'model/'))
 
     for i in range(epoches):
         trainer.train_epoch('train', train_dataset, collate_fn, batch_size, i, model_optimizer)
@@ -57,7 +57,7 @@ def train(args):
         early_stopping(loss_epoch, model, i) # 最良モデルならモデルパラメータ保存
         if early_stopping.early_stop: 
             break
-        trainer.serialize(i, output_path=base_output_path)
+        # trainer.serialize(i, output_path=base_output_path)
 
 def infer(args):
     batch_size = 1024
